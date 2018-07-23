49c9f3c92a112f86a7abebb3db45b5f479c3c708
Intengine <studio@intengine.pl>
2018-07-23 09:13:58 +0200

    Set one day forecast
    Programmed forecast for one day in one city in Poland.

--- WeatherApp/MainForm.Designer.cs
+++ WeatherApp/MainForm.Designer.cs
@@ -28,34 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelCityName = new System.Windows.Forms.Label();
+            this.labelCountryName = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelScaleName = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // labelCityName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.ForeColor = System.Drawing.Color.Gray;
-            this.label1.Location = new System.Drawing.Point(46, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(427, 47);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bielsko-Biała, Poland";
+            this.labelCityName.AutoSize = true;
+            this.labelCityName.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCityName.ForeColor = System.Drawing.Color.Gray;
+            this.labelCityName.Location = new System.Drawing.Point(46, 48);
+            this.labelCityName.Name = "labelCityName";
+            this.labelCityName.Size = new System.Drawing.Size(427, 47);
+            this.labelCityName.TabIndex = 0;
+            this.labelCityName.Text = "Bielsko-Biała, Poland";
             // 
-            // label2
+            // labelCountryName
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.ForeColor = System.Drawing.Color.Gray;
-            this.label2.Location = new System.Drawing.Point(48, 95);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 32);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Śląskie";
+            this.labelCountryName.AutoSize = true;
+            this.labelCountryName.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCountryName.ForeColor = System.Drawing.Color.Gray;
+            this.labelCountryName.Location = new System.Drawing.Point(48, 95);
+            this.labelCountryName.Name = "labelCountryName";
+            this.labelCountryName.Size = new System.Drawing.Size(104, 32);
+            this.labelCountryName.TabIndex = 1;
+            this.labelCountryName.Text = "Śląskie";
             // 
             // pictureBox1
             // 
@@ -67,29 +74,101 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // labelScaleName
+            // 
+            this.labelScaleName.AutoSize = true;
+            this.labelScaleName.Font = new System.Drawing.Font("Century Gothic", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelScaleName.ForeColor = System.Drawing.Color.Gray;
+            this.labelScaleName.Location = new System.Drawing.Point(160, 153);
+            this.labelScaleName.Name = "labelScaleName";
+            this.labelScaleName.Size = new System.Drawing.Size(77, 70);
+            this.labelScaleName.TabIndex = 3;
+            this.labelScaleName.Text = "C";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WeatherApp.Properties.Resources.CloudRain;
+            this.pictureBox2.Location = new System.Drawing.Point(488, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(68, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(560, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Days";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(562, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Conditions";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(562, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Description";
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.Color.Gray;
-            this.label3.Location = new System.Drawing.Point(160, 153);
+            this.label3.Location = new System.Drawing.Point(674, 153);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 70);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "C";
+            this.label3.Size = new System.Drawing.Size(114, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "12km/h";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.Gray;
+            this.label5.Location = new System.Drawing.Point(674, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 32);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "26";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.labelScaleName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelCountryName);
+            this.Controls.Add(this.labelCityName);
             this.Name = "MainForm";
             this.Text = "Intengine Weather";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,10 +176,16 @@
 
         #endregion
 
+        private System.Windows.Forms.Label labelCityName;
+        private System.Windows.Forms.Label labelCountryName;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelScaleName;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WeatherApp/MainForm.cs
+++ WeatherApp/MainForm.cs
@@ -1,25 +1,33 @@
 ﻿using System.Net;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WeatherApp
 {
     public partial class MainForm : Form
     {
-        const string APPID = "";
+        const string APPID = "401df202e10a68a5235956d31061bb5f";
         string cityName = "Bielsko";
 
         public MainForm()
         {
             InitializeComponent();
-            getWeather();
+            getWeather("Bielsko-Biała");
         }
 
-        void getWeather()
+        void getWeather(string city)
         {
             using(WebClient web = new WebClient())
             {
-                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q=Bielsko-Biała&appid=401df202e10a68a5235956d31061bb5f&units=metric&cnt=6");
+                string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric&cnt=6", city, APPID);
                 var json = web.DownloadString(url);
+                var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+
+                WeatherInfo.root outPut = result;
+
+                labelCityName.Text = string.Format("{0}", outPut.name);
+                labelCountryName.Text = string.Format("{0}", outPut.sys.country);
+                labelScaleName.Text = string.Format("{0} \u00B0" + "C", outPut.main.temp);
             }
         }
     }
